58a1c0dcaaa28cc00d081af0951ac91d79db75d8
yunuhenbm <yunuhen.badillo@gmail.com>
2025-02-12 16:36:36 -0800

    Ya quedó

--- badillo-yunuhen.py
+++ badillo-yunuhen.py
@@ -14,26 +14,35 @@ print("Comienza el juego 👀🦑")
 print(palabra_unida)
 print(f"La palabra oculta tiene {l} letras")
 print(f"Comientas con {vidas} vidas 💚💚💚💚💚") 
-letra = input("Ingrese una letra: ").lower().strip()
-
-#Validación de entrada
-while not (len(letra) == 1 and letra.isalpha()):
-    letra = input("Su entrada es inválida. Ingrese una letra: ").lower().strip()
 
 # Actualizar el ahorcado
 # ¿Está la letra en la palabra?
 #Para verificar si la letra está en la palabra, debemos recorrer la palabra. Para ello, será
 #Necesario un for in range
 #Aquí iría otro while
+while "_" in palabra_oc and vidas > 0:
+    letra = input("Ingrese una letra: ").lower().strip()
+    #Validación de entrada
+    while not (len(letra) == 1 and letra.isalpha()):
+        letra = input("Su entrada es inválida. Ingrese una letra: ").lower().strip()
 
-for i in range(len(palabra_sec)):
-    if palabra_sec[i] == letra:
-        palabra_sec[i] = letra
-        print("😍 Correcto, acertaste")
+    if letra in palabra_sec:
+        for i in range(len(palabra_sec)):
+            if palabra_sec[i] == letra:
+                palabra_oc[i] = letra
+        print("😍 Correcto, acertaste la letra")
         print(f"Tienes {vidas} vidas")
     else:
         print("La letra no se encuentra en la palabra. Vuelve a intentarlo")
         vidas -= 1
-print("".join(palabra_oc))
+    print("".join(palabra_oc))
+    print(f"Te quedan {vidas} vidas")
+
+#Fin del juego
+if "_" not in palabra_oc:
+    print("Ganaste el juego. ¡Felicidades!")
+else: 
+    print("Perdiste :( )")
+
 
 
